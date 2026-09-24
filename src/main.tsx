020ea3ac9937c251dcb70badb2ad22635152d7f8
Conrad Hansen-Quartey <Chansenq@gmail.com>
2026-09-24 14:53:20 +0000

    feat(auth): wrap app in Descope AuthProvider
    Add AuthProvider from @descope/react-sdk around
    <App /> in main.tsx, configured via
    VITE_DESCOPE_PROJECT_ID.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+// src/main.ts
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from "@descope/react-sdk";
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider projectId={import.meta.env.VITE_DESCOPE_PROJECT_ID}>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
